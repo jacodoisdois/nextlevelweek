f1a00bd4ae1e1ad9dd849c342dd755d69e8b9f98
Jacó <jacomagalhaes@gmail.com>
2020-10-13 22:29:26 -0300

    Segundo dia NLW

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {FiArrowRight} from 'react-icons/fi';
+import '../styles/pages/landing.css';
+import '../styles/global.css';
+import logoImg from '../images/logo.svg';
+import {Link} from 'react-router-dom';
+
+function Landing(){
+    return(
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+            <h1>Leve Felicidade Para o Mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+            
+        </main>
+        <div className="location">
+              <strong>Cidade de São Paulo</strong>
+              <span>São Paulo</span>
+        </div>
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0, 0.6)"/>  
+        </Link>
+      </div>
+    </div>
+    );
+}
+
+export default Landing;
